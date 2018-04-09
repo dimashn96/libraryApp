7769d5afd54353707fe7ade0dc1770c4726c677f
Dmitriy Shnitkovskiy <dima.shn96@gmail.com>
2018-04-09 12:13:02 +0300

    fix bugs

--- src/client/app/components/add-user/add-user.component.ts
+++ src/client/app/components/add-user/add-user.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { User } from '../../models/UserModel';
 import { Response } from '../../models/ResponseModel';
 import { HttpService } from '../../services/http.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-add-user',
@@ -14,11 +15,12 @@ export class AddUserComponent {
   message: string;
   done = false;
 
-  constructor(private httpService: HttpService) { }
+  constructor(private httpService: HttpService, private router: Router) { }
 
   submit() {
     this.httpService.addUser(this.user).subscribe((res: Response) => {
         this.done = true;
+        this.router.navigate(['auth']);
         if (typeof res.data === 'string') {
           this.message = res.data;
         }
